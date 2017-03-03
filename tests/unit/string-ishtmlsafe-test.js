b80392dc3316496ea823405e7214ee9f51dea713
Ryan Panning <panman82@gmail.com>
2017-03-02 19:44:30 -0600

    Move polyfill from a module import to "native" Ember.String.isHTMLSafe
    This takes a cue from `ember-getowner-polyfill` and
    `ember-assign-polyfill`.

--- tests/unit/string-ishtmlsafe-test.js
+++ tests/unit/string-ishtmlsafe-test.js
@@ -2,15 +2,11 @@ import { module, test } from 'qunit';
 import Ember from 'ember';
 import isHTMLSafe from 'ember-string-ishtmlsafe-polyfill';
 
-const {
-  htmlSafe
-} = Ember.String;
+module('Unit | isHTMLSafe');
 
-module('isHTMLSafe');
-
-test('unit tests', function(assert) {
+test('Imported isHTMLSafe() function properly detects safe strings', function(assert) {
   let plainStr = 'Hello World',
-      htmlSafeStr = htmlSafe(plainStr);
+      htmlSafeStr = Ember.String.htmlSafe(plainStr);
 
   assert.notOk(isHTMLSafe(plainStr), 'Plain string is not html safe string');
   assert.ok(isHTMLSafe(htmlSafeStr), 'Html safe string matches');
@@ -19,4 +15,17 @@ test('unit tests', function(assert) {
   assert.notOk(isHTMLSafe({}), 'Object is not html safe string');
   assert.notOk(isHTMLSafe(10), 'Number is not html safe string');
   assert.notOk(isHTMLSafe(null), 'Null is not html safe string');
+});
+
+test('"Native" Ember.String.isHTMLSafe() function properly detects safe strings', function(assert) {
+  let plainStr = 'Hello World',
+      htmlSafeStr = Ember.String.htmlSafe(plainStr);
+
+  assert.notOk(Ember.String.isHTMLSafe(plainStr), 'Plain string is not html safe string');
+  assert.ok(Ember.String.isHTMLSafe(htmlSafeStr), 'Html safe string matches');
+
+  assert.notOk(Ember.String.isHTMLSafe([]), 'Array is not html safe string');
+  assert.notOk(Ember.String.isHTMLSafe({}), 'Object is not html safe string');
+  assert.notOk(Ember.String.isHTMLSafe(10), 'Number is not html safe string');
+  assert.notOk(Ember.String.isHTMLSafe(null), 'Null is not html safe string');
 });
